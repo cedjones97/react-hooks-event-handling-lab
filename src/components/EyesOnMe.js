8470b6767878d014fcb7f2f1ee775853baa09f39
cedric <cedricj765@gmail.com>
2023-08-04 18:33:57 -0500

    updated lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -4,7 +4,7 @@ import React from 'react'
 function EyesOnMe() {
   return (
     <div>
-      <button onFocus={() => console.log('Good!')} onBlur={() => console.log('Hey! Eyes on me!') }>Eyes on me</button>
+      <button onFocus={() => console.log('Good!')} onBlur={() => console.log('Hey! Eyes on me!') }>Eyes On Me</button>
     </div>
   )
 }
